a8e93a2b838397a0725fc7eb96f3305123379ca1
redbeardy <redbeardymcgee@gmail.com>
2023-10-02 15:44:02 -0500

    should these be outside the fn? idk

--- src/components/NightbotAuthButton.js
+++ src/components/NightbotAuthButton.js
@@ -1,30 +1,32 @@
 import React from "react"
 import Button from "@mui/material/Button"
 
-export function NightbotAuthButton() {
-  const scope = [
-    "channel",
-    "commands",
-    "commands_default",
-    "regulars",
-    "song_requests",
-    "song_requests_queue",
-    "song_requests_playlist",
-    "spam_protection",
-    "subscribers",
-    "timers",
-  ].join(" ")
-  const params = new URLSearchParams({
-    client_id: "db258d5003bf9cef2316ccfd22aca141",
-    redirect_uri: "https://localhost:3000",
-    response_type: "token",
-    scope,
-    state: "FIXME",
-  })
-  const url = `https://api.nightbot.tv/oauth2/authorize?${params.toString()}`
+const SCOPE = [
+  "channel",
+  "commands",
+  "commands_default",
+  "regulars",
+  "song_requests",
+  "song_requests_queue",
+  "song_requests_playlist",
+  "spam_protection",
+  "subscribers",
+  "timers",
+].join(" ")
 
+const PARAMS = new URLSearchParams({
+  client_id: "db258d5003bf9cef2316ccfd22aca141",
+  redirect_uri: "https://localhost:3000",
+  response_type: "token",
+  scope: SCOPE,
+  state: "FIXME",
+})
+
+const URL = `https://api.nightbot.tv/oauth2/authorize?${PARAMS.toString()}`
+
+export function NightbotAuthButton() {
   return (
-    <Button variant="contained" href={url}>
+    <Button variant="contained" href={URL}>
       Authorize
     </Button>
   )
