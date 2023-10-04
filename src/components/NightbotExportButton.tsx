5b77efc6762ff5b88cb0e1827a0aad9515232d41
redbeardy <redbeardymcgee@gmail.com>
2023-10-04 17:35:20 -0500

    switch to named export

--- src/components/NightbotExportButton.tsx
+++ src/components/NightbotExportButton.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Button from "@mui/material/Button"
+import { Button } from "@mui/material"
 
 import { Headers, NightbotExportButtonProps } from "../../types/types"
 
